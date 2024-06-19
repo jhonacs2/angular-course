730bc6ff7c621dec907d4bc8d63975980ef9c273
Jhonatan Soto <jhonrocker2012@gmail.com>
2024-06-19 19:21:56 -0400

    02 angular-app init structure routes

--- 02-angular-app/link-app/src/app/app.module.ts
+++ 02-angular-app/link-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -15,4 +15,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
