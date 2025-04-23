dbf98ac1a36cc415f9614c2c5f6de100db137c1f
Jhonatan Soto <jhonrocker2012@gmail.com>
2025-04-23 19:28:19 -0400

    initial structure todo-app

--- 07-angular-workspace/angular-workspace/projects/todo-app/src/app/app.module.ts
+++ 07-angular-workspace/angular-workspace/projects/todo-app/src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NewTodoComponent} from './todos/components/new-todo/new-todo.component';
+import {TodoComponent} from './todos/components/todo/todo.component';
+import {FooterComponent} from './todos/components/footer/footer.component';
+import { TodoListComponent } from './todos/components/todo-list/todo-list.component';
+import { TodoListItemComponent } from './todos/components/todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTodoComponent,
+    TodoComponent,
+    FooterComponent,
+    TodoListComponent,
+    TodoListItemComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ 07-angular-workspace/angular-workspace/projects/todo-app/src/app/todos/components/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-item',
+  templateUrl: './todo-list-item.component.html',
+  styleUrls: ['./todo-list-item.component.scss']
+})
+export class TodoListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
